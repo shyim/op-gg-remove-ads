89ef936e7ed77d6f40ca1424104dd8c85d9eebf3
Soner Sayakci <s.sayakci@shopware.com>
2023-03-27 17:07:19 +0200

    fix: insert js only in js files

--- worker.js
+++ worker.js
@@ -13,10 +13,13 @@ export default {
         let content = await res.text();
   
         content = content.replaceAll('https://opgg-desktop-data.akamaized.net', "https://op-gg-remove-ads.shyim.workers.dev");
-        content = content.replaceAll(/<body>.*<\/body>/gm, 'TEST');
-        content = content.replaceAll('https://www.mobwithad.com', 'https://google.com');
-        content += '\ndocument.head.insertAdjacentHTML("beforeend", \'<style>#ads-container,#ads-container2,#ads-container3,#sids-ads{display: none !important}</style>\')';
-  
+
+        if (pathName.endsWith('.js')) {
+            content = content.replaceAll(/<body>.*<\/body>/gm, '');
+            content = content.replaceAll('https://www.mobwithad.com', 'https://google.com');
+            content += '\ndocument.head.insertAdjacentHTML("beforeend", \'<style>#ads-container,#ads-container2,#ads-container3,#sids-ads{display: none !important}</style>\')';
+        }
+
         return new Response(content, {
           statusCode: res.statusCode,
           headers
